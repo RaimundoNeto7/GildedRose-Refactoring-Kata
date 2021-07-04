365febf6f4764b44f966cbf95436e395a4bf2735
Raimundo Neto <raimundoneto.rn17@gmail.com>
2021-07-03 23:19:43 -0400

    Change statement ifs

--- python/src/gilded_rose.py
+++ python/src/gilded_rose.py
@@ -19,25 +19,25 @@ class GildedRose(object):
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
 
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                item.quality = item.quality - 1
-            else:
+            item.sell_in = item.sell_in - 1
+
+            if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert":
                 item.quality = item.quality + 1
                 if item.name == "Backstage passes to a TAFKAL80ETC concert":
                     if item.sell_in < 11:
                         item.quality = item.quality + 1
                     if item.sell_in < 6:
                         item.quality = item.quality + 1
+                    if item.sell_in < 0:
+                        item.quality = 0
+                if item.sell_in < 0:
+                    if item.name == "Aged Brie":
+                        item.quality = item.quality + 1
+            else:                
+                item.quality = item.quality - 1
 
-            item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    item.quality = item.quality + 1
+                if item.sell_in < 0:
+                    item.quality = item.quality - 1
             
             item.quality = min(50, item.quality)
             item.quality = max(0, item.quality)
