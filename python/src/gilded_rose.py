c0ae1c55981b51b05750ca4e251f74378c7aae9b
Raimundo Neto <raimundoneto.rn17@gmail.com>
2021-07-03 23:31:55 -0400

    Split Backstage and Brie statements

--- python/src/gilded_rose.py
+++ python/src/gilded_rose.py
@@ -19,25 +19,28 @@ class GildedRose(object):
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
 
-            item.sell_in = item.sell_in - 1
+            item.sell_in -= 1
 
-            if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert":
-                item.quality = item.quality + 1
-                if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                    if item.sell_in < 11:
-                        item.quality = item.quality + 1
-                    if item.sell_in < 6:
-                        item.quality = item.quality + 1
-                    if item.sell_in < 0:
-                        item.quality = 0
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
                 if item.sell_in < 0:
-                    if item.name == "Aged Brie":
-                        item.quality = item.quality + 1
+                    item.quality = 0
+                elif item.sell_in <= 5:
+                    item.quality += 3
+                elif item.sell_in <= 10:
+                    item.quality += 2
+                else:
+                    item.quality += 1
+
+            elif item.name == "Aged Brie":
+                item.quality += 1                    
+                if item.sell_in < 0:
+                    item.quality += 1
+                        
             else:                
-                item.quality = item.quality - 1
+                item.quality -= 1
 
                 if item.sell_in < 0:
-                    item.quality = item.quality - 1
+                    item.quality -= 1
             
             item.quality = min(50, item.quality)
             item.quality = max(0, item.quality)
