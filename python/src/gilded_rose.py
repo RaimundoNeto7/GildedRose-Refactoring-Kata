1a69568a3241e693fe5d3340da9218e99df41647
Raimundo Neto <raimundoneto.rn17@gmail.com>
2021-07-03 22:20:09 -0400

    Move giled_rose modulo to src

--- python/src/gilded_rose.py
+++ python/src/gilded_rose.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 
+class Item:
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def __repr__(self):
+        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
 class GildedRose(object):
 
     def __init__(self, items):
@@ -34,13 +43,3 @@ class GildedRose(object):
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
-
-
-class Item:
-    def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
-
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
