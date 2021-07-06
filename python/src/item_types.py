243d8164cf5535aa02885e464941be2e0b49401b
Raimundo Neto <raimundoneto.rn17@gmail.com>
2021-07-06 06:38:17 -0400

    Add enum ItemTypes

---
+++ python/src/item_types.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class ItemTypes(Enum):
+    SULFURAS = "Sulfuras, Hand of Ragnaros"
+    BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+    AGED_BRIE = "Aged Brie"
+    MAGICALLY_CONJURED = "Magically Conjured"
